4bc13a7f942117ed66db4e3c72bcb4e68faa22d4
lahumisal <lahumisal402@gmail.com>
2026-01-10 00:43:55 +0530

    remove dependency and correct prot for redirect

--- src/main/java/com/smart_school/smart_school_management/SmartSchoolManagementApplication.java
+++ src/main/java/com/smart_school/smart_school_management/SmartSchoolManagementApplication.java
@@ -2,7 +2,6 @@ package com.smart_school.smart_school_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SmartSchoolManagementApplication {
