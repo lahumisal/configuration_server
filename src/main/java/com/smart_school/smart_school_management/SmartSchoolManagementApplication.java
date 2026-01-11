eb858250d0f00c71647aa73add9ac6777c4a84ce
lahumisal <lahumisal402@gmail.com>
2026-01-11 14:47:14 +0530

    correct yml configuration server microservice

--- src/main/java/com/smart_school/smart_school_management/SmartSchoolManagementApplication.java
+++ src/main/java/com/smart_school/smart_school_management/SmartSchoolManagementApplication.java
@@ -2,7 +2,9 @@ package com.smart_school.smart_school_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class SmartSchoolManagementApplication {
 
